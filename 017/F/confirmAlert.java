2e55902e7b8c95447c982e04a7d439cd831f8306
@joaopaulom_dev <joaopaulomsilva2021@outlook.com>
2022-07-25 21:31:27 -0300

    Aula 081 | 2022-07-25

---
+++ 017/F/confirmAlert.java
@@ -0,0 +1,62 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.AWTEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class confirmAlert extends JFrame{
+	
+	public confirmAlert(String title, String message, Dimension windowSize)
+	{
+		//Configura janela
+		super();
+		setTitle(title);
+		setSize(windowSize);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		
+		//Posiciona janela
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((screenSize.width/2)-(windowSize.width/2),(screenSize.height/2)-(windowSize.height/2));
+		
+		//Cria elementos
+		JLabel alertText = new JLabel(message, JLabel.CENTER);
+		alertText.setBounds(200, 100, 200, 20);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setBounds(200, 160, 80, 40);
+		
+		JButton btnErro = new JButton("ERRO");
+		btnErro.setBounds(320, 160, 80, 40);
+		
+		//Cria eventos
+		btnOk.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent event)
+			{
+				//Apresenta sucesso
+				System.out.println("Operacao realizada com sucesso.");
+			}
+		});
+		
+		btnErro.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent event)
+			{
+				//Apresenta erro
+				System.out.println("Operacao cancelada.");
+				System.exit(0);
+			}
+		});
+		
+		//Apresenta janela
+		add(alertText);
+		add(btnOk);
+		add(btnErro);
+		
+		setVisible(true);
+	}
+}
---
+++ 017/F/f.java
@@ -0,0 +1,10 @@
+import java.awt.Dimension;
+
+public class f{
+	
+	public static void main(String[] args){
+		
+		//Apresenta alerta
+		confirmAlert alertWindow = new confirmAlert("Alerta", "Deseja realizar o seguinte?", new Dimension(600, 300));
+	}
+}
